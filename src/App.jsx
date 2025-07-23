2e0c9d894398c5fa26249d4f481943fa0704c03d
Kunal Vishwakarma <kunalvishwakarma2903@gmail.com>
2025-07-24 01:54:54 +0530

    Initial commit from new account

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from './sections/Navbar';
+import Hero from './sections/Hero';
+import About from './sections/About';
+import Projects from './sections/Projects';
+import Experiences from './sections/Experiences';
+import Testimonial from './sections/Testimonial';
+import Contact from './sections/Contact';
+import Footer from './sections/Footer';
+import FluidCursor from './components/FluidCursor'; // ✅ Make sure the path is correct
+
+const App = () => {
+  return (
+    <>
+      <FluidCursor /> {/* ✅ Canvas overlay for fluid cursor */}
+
+      <div className="container mx-auto max-w-7xl">
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
+        <Experiences />
+        <Testimonial />
+        <Contact />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
