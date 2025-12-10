efc8689d2cfd3545529a95ff342497c30e42873b
Kunal Vishwakarma <kunalvishwakarma20@gmail.com>
2025-12-10 07:37:25 +0530

    Initial commit: Portfolio project

---
+++ src/constants/index.js
@@ -0,0 +1,334 @@
+export const myProjects = [
+  // {
+  //   id: 1,
+  //   title: "E-commerce Platform",
+  //   description:
+  //     "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+  //   subDescription: [
+  //     "Built a scalable application with ASP.NET Core MVC, integrating global platforms like Amazon for domestic delivery.",
+  //     "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+  //     "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+  //     "Added payment systems, localization, and product filtering for functionality improvements.",
+  //   ],
+  //   href: "https://hazuku.com",
+  //   logo: "",
+  //   image: "/assets/projects/hazuku.png",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "Shopify",
+  //       path: "/assets/logos/csharp.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Liquidity",
+  //       path: "/assets/logos/dotnet.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Html",
+  //       path: "/assets/logos/efcore.png",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "Javascript",
+  //       path: "/assets/logos/tailwindcss.svg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 2,
+  //   title: "Authentication & Authorization System",
+  //   description:
+  //     "A secure authentication and authorization system using Auth0 for seamless user management.",
+  //   subDescription: [
+  //     "Integrated Auth0 for authentication, supporting OAuth, JWT, and multi-factor authentication.",
+  //     "Implemented role-based access control (RBAC) for fine-grained user permissions.",
+  //     "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
+  //     "Connected to a secure SQLite database for user data storage.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/auth-system.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "Auth0",
+  //       path: "/assets/logos/auth0.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "React",
+  //       path: "/assets/logos/react.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "SQLite",
+  //       path: "/assets/logos/sqlite.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "TailwindCSS",
+  //       path: "/assets/logos/tailwindcss.svg",
+  //     },
+  //   ],
+  // },
+  {
+    id: 3,
+    title: "KVTV - Movie Streaming Web App",
+    description:
+      "A modern, feature-rich movie and series streaming platform offering a highly interactive user experience.",
+    subDescription: [
+      "React Router handled client-side navigation for fast switching between content pages and seamless routing.",
+      "Context API managed dynamic state for personalized watchlists and viewing history across the application.",
+      "Tailwind CSS crafted responsive, cinematic UI with dynamic hero banners and accessible carousel components.",
+      "Integrated video player component supporting adaptive streaming (HLS/DASH) for optimized content delivery.",
+    ],
+    href: "https://kvtv.vercel.app/",
+    logo: "",
+    image: "/assets/projects/kvtv.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/blazor.svg",
+      },
+      {
+        id: 2,
+        name: "API Integration",
+        path: "/assets/logos/dotnetcore.svg",
+      },
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "Vite.js",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  
+  {
+    id: 5,
+    title: "Masterone - Productivity One Stop Solution",
+    description:
+      "A full-stack productivity platform built with Next.js, TypeScript, Tailwind, and Clerk Auth—designed to give founders a fast and customizable all-in-one workspace.",
+    subDescription: [
+      "Masterone is a modern productivity hub built with Next.js and TypeScript, optimized for speed, security, and scalability.",
+      "It features a clean, responsive UI powered by Tailwind CSS for a smooth and customizable experience.",
+      "Clerk authentication ensures secure access, seamless onboarding, and role-based control for teams.",
+      "Designed for founders, it centralizes tasks, notes, and workflows into one powerful, efficient platform.",
+    ],
+    href: "https://masterone.vercel.app/",
+    logo: "",
+    image: "/assets/projects/masterone.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "Typescript",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "Clerk",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "MongoDB",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Codereviewer - Ai Code Reviewer & Generator",
+    description:
+      "An AI-powered platform designed to review and generate code.",
+    subDescription: [
+      "Built a powerful AI analysis engine using Gemini Api for code review and generation.",
+      "Developed a real-time code reviewing system with contextual feedback and suggestion generation",
+      "Implemented a language-agnostic parsing system for easy codebase compatibility.",
+      "Optimized performance with distributed computing and efficient token processing.",
+    ],
+    href: "https://coderevieweee.netlify.app/",
+    logo: "",
+    image: "/assets/projects/codereviewer.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/cplusplus.svg",
+      },
+      {
+        id: 2,
+        name: "Gsap",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "Spline",
+        path: "/assets/logos/microsoft.svg",
+      },
+    ],
+  },
+  // {
+  //   id: 6,
+  //   title: "Online Learning Platform",
+  //   description:
+  //     "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+  //   subDescription: [
+  //     "Built using Blazor WebAssembly for a seamless SPA experience.",
+  //     "Implemented video streaming with Azure Media Services.",
+  //     "Added a quiz system with dynamic question generation and real-time grading.",
+  //     "Integrated Stripe API for secure payment processing.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/elearning.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "Blazor",
+  //       path: "/assets/logos/blazor.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Azure",
+  //       path: "/assets/logos/azure.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Stripe",
+  //       path: "/assets/logos/stripe.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "TailwindCSS",
+  //       path: "/assets/logos/tailwindcss.svg",
+  //     },
+  //   ],
+  // },
+];
+
+export const mySocials = [
+  {
+    name: "GitHub",
+    href: "https://github.com/kunalvish20/",
+    icon: "/assets/logos/github.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/kunal-vishwakarma-aa5056366/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Twitter",
+    href: "https://x.com/kunalvish20?s=21",
+    icon: "twitter", // Will use inline SVG
+  },
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/918827739944",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+];
+export const experiences = [
+  {
+    title: "Freelance Frontend Developer & UI/UX Designer",
+    job: "Self-Employed",
+    date: "2023",
+    contents: [
+      "Designed and developed responsive UIs for personal and client projects using Figma, Tailwind CSS, and React.",
+      "Built engaging frontend experiences with animations using GSAP and Framer Motion.",
+      "Created reusable UI component libraries and contributed to open-source design systems.",
+      "Focused on improving design thinking and user-centric workflows through freelance work.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    job: "Globussoft Technologies",
+    date: "2024–Present",
+    contents: [
+      "Developing scalable full-stack applications using Node.js, Express, MongoDB, and React.",
+      "Engineered APIs and backend logic for SaaS platforms and enterprise applications.",
+      "Collaborated with cross-functional teams to build features in a CI/CD environment.",
+      "Integrated AI tools, authentication systems, and real-time features in production apps.",
+    ],
+  },
+  {
+    title: "Full-Stack Developer",
+    job: "Personal Projects & Learning",
+    date: "2024–Present",
+    contents: [
+      "Building diverse web platforms such as multilingual news portals and matchmaking systems.",
+      "Experimenting with Three.js, shaders, canvas animations, and advanced React patterns.",
+      "Learning system design, cloud deployment, and backend infrastructure with a focus on performance.",
+      "Documenting and sharing open-source contributions and projects on GitHub and portfolio.",
+    ],
+  },
+];
+
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
